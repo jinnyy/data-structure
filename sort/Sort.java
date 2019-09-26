b4ca55a5a61aca6a6ec7fc93ec7092c568c30011
Yeonjin Kim <jinny309@yonsei.ac.kr>
2019-09-26 14:26:47 +0900

    [Sort] add quickSort() method

--- sort/Sort.java
+++ sort/Sort.java
@@ -35,10 +35,37 @@ public class Sort {
 		return sorted;
 	}
 	
+	// quick sort
+	static void quickSort(int[] arr) {
+		quickSort(arr, 0, arr.length-1);
+	}
+	
+	static void quickSort(int arr[], int low, int high) {
+		if (low < high) { 
+			int pivot = partition(arr, low, high);
+			
+			quickSort(arr, low, pivot-1);
+			quickSort(arr, pivot+1, high);
+		}
+	}
+
+	static int partition(int arr[], int low, int high) {
+		int pivot = arr[high];  
+		int loc = low; // index of smaller element 
+		for (int j=low; j<high; j++) { 
+			if (arr[j] < pivot) { 
+				swap(arr, loc, j);
+				loc++;
+			}
+		}
+		swap(arr, loc, high);
+		return loc; 
+	}
+	
 	private static void swap(int[] arr, int idx1, int idx2) {
 		int temp = arr[idx1];
 		arr[idx1] = arr[idx2];
 		arr[idx2] = temp;
 	}
-
+	
 }
